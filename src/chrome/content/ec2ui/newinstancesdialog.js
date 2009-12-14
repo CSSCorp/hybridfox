343054a9a62a5cd501efa20a41754cf06974ba74
sailing.instructor <devnull@localhost>
2009-12-14 17:16:44 +0000

    Added: If the region is EC2 (i.e. region is 'us' or 'eu'), disable the public/private checkbox on the "New Instance(s)" Dialog.

--- src/chrome/content/ec2ui/newinstancesdialog.js
+++ src/chrome/content/ec2ui/newinstancesdialog.js
@@ -265,6 +265,11 @@ var ec2_InstanceLauncher = {
             // editable menulists
         }
 
+        //Since EC2 doesn't support private addressing, disable it when the region is US or EU.
+        if (region.search(/^us$/i) != -1 || region.search(/^eu$/i) != -1 ) {
+        	document.getElementById("ec2ui.newinstances.addressingType").disabled="true";
+        }
+        
         this.refreshDisplay();
     },
 
