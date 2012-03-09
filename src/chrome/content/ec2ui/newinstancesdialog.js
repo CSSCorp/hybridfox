8dbf50597b2f314b18c73db413dee3175912d437
anandchitravelu <devnull@localhost>
2012-03-09 15:27:19 +0530

    Adding new Instance type "m1.medium"

--- src/chrome/content/ec2ui/newinstancesdialog.js
+++ src/chrome/content/ec2ui/newinstancesdialog.js
@@ -273,25 +273,32 @@ var ec2_InstanceLauncher = {
             //Just checking for EC2 or not
             if (region.type == "ec2") {
 		if(rootdevice == "ebs"){
-                typeMenu.appendItem("t1.micro", "t1.micro");
-                typeMenu.appendItem("m1.large", "m1.large");
-                typeMenu.appendItem("m1.xlarge", "m1.xlarge");
-                typeMenu.appendItem("c1.xlarge", "c1.xlarge");
-                typeMenu.appendItem("m2.xlarge", "m2.xlarge");
-                typeMenu.appendItem("m2.2xlarge", "m2.2xlarge");
-                typeMenu.appendItem("m2.4xlarge", "m2.4xlarge");
-                typeMenu.appendItem("cc1.4xlarge", "cc1.4xlarge");
-                typeMenu.appendItem("cg1.4xlarge", "cg1.4xlarge");
+                   typeMenu.appendItem("t1.micro", "t1.micro");
+				   typeMenu.appendItem("c1.medium", "c1.medium");
+				   typeMenu.appendItem("m1.medium", "m1.medium");	 	
+                   typeMenu.appendItem("m1.large", "m1.large");
+                   typeMenu.appendItem("m1.xlarge", "m1.xlarge");
+                   typeMenu.appendItem("c1.xlarge", "c1.xlarge");
+                   typeMenu.appendItem("m2.xlarge", "m2.xlarge");
+                   typeMenu.appendItem("m2.2xlarge", "m2.2xlarge");
+                   typeMenu.appendItem("m2.4xlarge", "m2.4xlarge");
+				   typeMenu.appendItem("cc1.4xlarge", "cc1.4xlarge");
+				   typeMenu.appendItem("cc2.8xlarge", "cc2.8xlarge");
+                   typeMenu.appendItem("cg1.4xlarge", "cg1.4xlarge");
 		}
 		else {
-                typeMenu.appendItem("m1.large", "m1.large");
-                typeMenu.appendItem("m1.xlarge", "m1.xlarge");
-                typeMenu.appendItem("c1.xlarge", "c1.xlarge");
-                typeMenu.appendItem("m2.xlarge", "m2.xlarge");
-                typeMenu.appendItem("m2.2xlarge", "m2.2xlarge");
-                typeMenu.appendItem("m2.4xlarge", "m2.4xlarge");
-                typeMenu.appendItem("cc1.4xlarge", "cc1.4xlarge");
-                typeMenu.appendItem("cg1.4xlarge", "cg1.4xlarge");
+                   typeMenu.appendItem("m1.small", "m1.small");
+				   typeMenu.appendItem("c1.medium", "c1.medium");
+				   typeMenu.appendItem("m1.medium", "m1.medium");
+				   typeMenu.appendItem("m1.large", "m1.large");
+                   typeMenu.appendItem("m1.xlarge", "m1.xlarge");
+                   typeMenu.appendItem("c1.xlarge", "c1.xlarge");
+                   typeMenu.appendItem("m2.xlarge", "m2.xlarge");
+                   typeMenu.appendItem("m2.2xlarge", "m2.2xlarge");
+                   typeMenu.appendItem("m2.4xlarge", "m2.4xlarge");
+                   typeMenu.appendItem("cc1.4xlarge", "cc1.4xlarge");
+				   typeMenu.appendItem("cc2.8xlarge", "cc2.8xlarge");
+                   typeMenu.appendItem("cg1.4xlarge", "cg1.4xlarge");
             }
         }
             else {
@@ -306,13 +313,21 @@ var ec2_InstanceLauncher = {
             //Just checking for EC2 or not
             if (region.type == "ec2") {
 		if(rootdevice == "ebs"){
-		typeMenu.appendItem("t1.micro", "t1.micro");
-                typeMenu.appendItem("m1.small", "m1.small");
-                typeMenu.appendItem("c1.medium", "c1.medium");  
+		           typeMenu.appendItem("t1.micro", "t1.micro");
+                   typeMenu.appendItem("m1.small", "m1.small");
+                   typeMenu.appendItem("c1.medium", "c1.medium");
+				   typeMenu.appendItem("m1.medium", "m1.medium"); 
 		}
 		else{
-		typeMenu.appendItem("m1.small", "m1.small");
-                typeMenu.appendItem("c1.medium", "c1.medium"); 
+		           typeMenu.appendItem("m1.small", "m1.small");
+				   typeMenu.appendItem("c1.medium", "c1.medium");
+				   typeMenu.appendItem("m1.medium", "m1.medium");
+				   typeMenu.appendItem("m1.large", "m1.large");
+                   typeMenu.appendItem("m1.xlarge", "m1.xlarge");
+                   typeMenu.appendItem("c1.xlarge", "c1.xlarge");
+                   typeMenu.appendItem("m2.xlarge", "m2.xlarge");
+                   typeMenu.appendItem("m2.2xlarge", "m2.2xlarge");
+                   typeMenu.appendItem("m2.4xlarge", "m2.4xlarge"); 
 		}
 	    }
             else { 
